fbd5df5fed066e6f66f6b45dc3417fdb8db203ee
CryptoVinnie <Cryptovin@pm.me>
2018-12-03 02:47:53 +0000

    main.go

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
-#main.go
+package main
+
+import (
+"fmt"    
+"os/exec"
+"bytes"
+)
+
+func main() {
+	cmd := exec.Command("skycoin-cli", "addressGen")
+
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	//	Run the command
+	cmd.Run()
+
+	//	Output our results
+	fmt.Printf("Result: %v / %v", out.String(), stderr.String())
+}
